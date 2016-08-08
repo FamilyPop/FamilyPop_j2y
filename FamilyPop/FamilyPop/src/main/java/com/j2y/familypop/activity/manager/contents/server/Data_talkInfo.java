9032b2359d9584b19dc79a7dcbb9b473186c3f00
sijmark <nanl13n@j2ysoft.com>
2016-08-08 13:39:25 +0900

    160808

---
+++ FamilyPop/FamilyPop/src/main/java/com/j2y/familypop/activity/manager/contents/server/Data_talkInfo.java
@@ -0,0 +1,47 @@
+package com.j2y.familypop.activity.manager.contents.server;
+
+import com.j2y.familypop.activity.manager.Manager_actor;
+import com.j2y.familypop.activity.manager.actors.Actor_attractor;
+import com.j2y.familypop.activity.manager.actors.BaseActor;
+
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Created by lsh on 2016-08-07.
+ */
+// todo : 토크 모드 일때 필요한 정보를 가져 오는 클레스. (Clam 도 여기서 계산 하게 이동 하자)
+//public class Data_talkInfo
+//{
+//    public class talkInfo_attractor
+//    {
+//        Actor_attractor _object;
+//
+//
+//
+//        public talkInfo_attractor()
+//        {
+//
+//        }
+//    }
+//
+//
+//    private Manager_actor _magActor = null;
+//    private CopyOnWriteArrayList<BaseActor> _talks = null;
+//    private CopyOnWriteArrayList<BaseActor> _attractors = null;
+//    //
+//
+//
+//    public Data_talkInfo()
+//    {
+//        _magActor = Manager_actor.Instance;
+//        _talks =  Manager_actor.Instance.GetActorsList(Manager_actor.eType_actor.ACTOR_TALK);
+//        _attractors = Manager_actor.Instance.GetActorsList(Manager_actor.eType_actor.ACTOR_ATTRACTOR);
+//    }
+//
+//    private void Calculation_talkRatiosPair()
+//    {
+//
+//
+//    }
+//}
