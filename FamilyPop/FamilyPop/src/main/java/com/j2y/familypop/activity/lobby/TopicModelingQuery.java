be35d6d35c7041cce04c69f878346b61e9e4e1ac
JungiJeong <pangye@kaist.ac.kr>
2016-10-12 18:34:11 +0900

    TopicModeling 다시 만들기

--- FamilyPop/FamilyPop/src/main/java/com/j2y/familypop/activity/lobby/TopicModelingQuery.java
+++ FamilyPop/FamilyPop/src/main/java/com/j2y/familypop/activity/lobby/TopicModelingQuery.java
@@ -2,6 +2,7 @@ package com.j2y.familypop.activity.lobby;
 
 import android.content.ContentValues;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -15,6 +16,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 /**
  * Created by KJW on 2016-04-25.
@@ -24,11 +27,16 @@ import java.net.URLEncoder;
 
 public class TopicModelingQuery extends AsyncTask<String, Void, String> {
     String strEventType;
-    String strEventQuery;
+    Activity_topicGallery.QueryThread queryThread = null;
 
-    public TopicModelingQuery(String eventType, String eventQuery) {
+    private final String topicDelim = ",,,,,";
+    private final String postDelim = "!#!#!#";
+
+    public TopicModelingQuery(Activity_topicGallery.QueryThread queryThread, String eventType) {
         this.strEventType = eventType;
-        this.strEventQuery = eventQuery;
+        this.queryThread = queryThread;
+
+        Log.i("TopicModeling", "TopicModeling Query created with users selected: " + eventType);
     }
 
     @Override
@@ -44,11 +52,49 @@ public class TopicModelingQuery extends AsyncTask<String, Void, String> {
 
     // onPostExecute displays the results of the AsyncTask.
     @Override
-    protected void onPostExecute(String result) {
+    protected void onPostExecute(String query_result) {
+        ArrayList<String> keywords = new ArrayList<String> ();
+        ArrayList<String> relatedPosts = new ArrayList<String> ();
+        ArrayList<String> textInPost = new ArrayList<String> ();
+
+        // Parse the result
+        String[] sets = query_result.split("##########");
+        String[] topicSet, postSet;
+
+        //for (int i=0; i<sets.length; i++)
+        //    Log.i("TopicModeling", sets[i]);
+        //Log.i("TopicModeling", "length: " + query_result.length());
+
+        //  Parse the keyword sets
+        topicSet = sets[0].split("///");
+        for (int i=0; i<topicSet.length; i++)
+        {
+            StringTokenizer topics = new StringTokenizer(topicSet[i], topicDelim);
+            while (topics.hasMoreTokens())
+                keywords.add(topics.nextToken());
+        }
+
+        //  Parse the related posts
+        postSet = sets[1].split("///");
+        for (int k=0; k<postSet.length; k++)
+        {
+            String[] posts = postSet[k].split(topicDelim);
+            for (int i=0; i<posts.length; i++)
+            {
+                String[] tk = posts[i].split(postDelim);
+                relatedPosts.add(tk[0]);
+                textInPost.add(tk[1]);
+            }
+        }
+
+        Log.i("TopicModeling", keywords.toString());
+        Log.i("TopicModeling", relatedPosts.toString());
+        Log.i("TopicModeling", textInPost.toString());
+
+        queryThread.displayQueryResult(keywords, relatedPosts, textInPost);
     }
 
-        /*Connection Module*/
-
+    /*Connection Module*/
     private String downloadUrl(String myurl) throws IOException {
         InputStream is = null;
         // Get 65,535 characters of the retrieved web page content.
@@ -60,6 +106,7 @@ public class TopicModelingQuery extends AsyncTask<String, Void, String> {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setReadTimeout(10000 /* milliseconds */);
             conn.setConnectTimeout(15000 /* milliseconds */);
+            Log.i("TopicModeling", "Connection to Topic Modeling Server established.");
 
             conn.setRequestMethod("POST");
             conn.setDoOutput(true);
@@ -89,8 +136,10 @@ public class TopicModelingQuery extends AsyncTask<String, Void, String> {
             int response = conn.getResponseCode();
             is = conn.getInputStream();
 
+            Log.i("TopicModeling", "Response from Topic Modeling Server received.");
+
             // Convert the InputStream into a string
-            String contentAsString = readIt(is, len);
+            String contentAsString = readIt(is);
             return contentAsString;
 
             // Makes sure that the InputStream is closed after the app is
@@ -102,9 +151,8 @@ public class TopicModelingQuery extends AsyncTask<String, Void, String> {
         }
     }
 
-    public String readIt(InputStream stream, int len) throws IOException {
-        Reader reader = null;
-        reader = new InputStreamReader(stream, "UTF-8");
+    public String readIt(InputStream stream) throws IOException {
+        Reader reader = new InputStreamReader(stream, "UTF-8");
         BufferedReader bufferedReader = new BufferedReader(reader);
 
         String ret;
@@ -113,10 +161,6 @@ public class TopicModelingQuery extends AsyncTask<String, Void, String> {
             content += ret;
 
         return content;
-        //char[] buffer = new char[len];
-        //reader.read(buffer);
-        //reader.read(buffer, 0, len);
-        //return new String(buffer);
     }
 }
 
